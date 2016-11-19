bf716d6d74abcd168442c44cc3fa6c86f6b0b4ad
catcat520 <catcat520@users.noreply.github.com>
2016-11-19 13:14:31 +0800

    Update 疑难杂症.js

--- Fonts/CSS/疑难杂症.js
+++ Fonts/CSS/疑难杂症.js
@@ -1,4 +1,4 @@
-001.chrome 字体 粗细 FallBack
+001.修正 chrome 字体 粗细渲染 FallBack
 #ThePowerOfRedYellow{content:"手札体-简体";}
 @font-face{font-family:"Plus.Func.Fonts.CN.FallBack";font-weight:Bold;src:local("Hannotate SC"),local("Hannotate SC Regular");}
 @font-face{font-family:"Plus.Func.Fonts.CN.FallBack";src:local("Hannotate SC"),local("Hannotate SC Regular");}
